6b531e52596365eedd226bc5716cb38d881e083c
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2022-09-09 15:23:25 +0300

    Remove template. Add header.

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Choose date. Work with notes.</h1>
+</template>
+
+<script>
+  export default {
+    name: 'MyHeader',
+    data() {
+      return {
+        
+      };
+    }
+  };
+</script>
+
+<style>
+  h1 {
+    font-size: 28px;
+    color: rebeccapurple;
+  }
+</style>
