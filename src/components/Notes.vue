7d111825c15ee0ac906bfcab5f853a71be96edc9
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2022-09-16 00:30:20 +0300

    Add sugar to Notes.

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,6 +1,6 @@
 <template>
   <h2>Notes</h2>
-  <textarea name="" id="currentNote" cols="30" rows="10" v-focus></textarea>
+  <textarea name="" id="currentNote" cols="30" rows="10"></textarea>
   <button class="btn" v-on:click="saveNote()">Save.</button>
   <ul class="" v-for="note in notes">
     <li class="">{{ note }}</li>
@@ -8,35 +8,16 @@
   <p id="end">Not implemented.</p>
 </template>
 
-<script>
+<script setup>
   import { reactive } from "vue";
-  export default {
-    props: {
-      
-    },
-    name: 'Notes',
-    directives: {
-      focus: {
-        mounted(el) {
-          el.focus()
-        }
-      },
-    },
-    setup(props, context) {
-      const notes = reactive(new Map());
-      const saveNote = () => {
-        if (!currentNote.value.length) return;
+  
+  const notes = reactive(new Map());
+  const saveNote = () => {
+    if (!currentNote.value.length) return;
         
-        const time = new Date().toTimeString().slice(0, 8);
-        notes.set(time, currentNote.value);
-        console.log(Array.from(notes));
-      };
-      
-      return {
-        notes,
-        saveNote,
-      }
-    },
+    const time = new Date().toTimeString().slice(0, 8);
+    notes.set(time, currentNote.value);
+    console.log(Array.from(notes));
   };
 </script>
 
